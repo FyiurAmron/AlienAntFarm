848bcca24ee3d4816a45b277ec8870bb98605974
toor <toor@Cisco1>
2014-06-16 20:18:37 +0200

    * v0.2

--- src/vax/alienantfarm/constant.java
+++ src/vax/alienantfarm/constant.java
@@ -8,7 +8,7 @@ public interface constant {
   int STRIDE = 14;
   double GENE_MIN = 0, GENE_MAX = 1,
           MIX_MIN = 0.25, MIX_MAX = 0.75, MIX_RANGE = MIX_MAX - MIX_MIN,
-          BASE_MUT_MAX = 0.1, BASE_MUT_RANGE = 2 * BASE_MUT_MAX,
+          BASE_MUT_MAX = 0.01, BASE_MUT_RANGE = 2 * BASE_MUT_MAX,
           BASE_RND_MIN = 0.25, BASE_RND_MAX = 0.75, BASE_RND_RANGE = BASE_RND_MAX - BASE_RND_MIN;
   double PHERO_DISP_RATE = 0.9, PHERO_STRIDE_SPREAD = 2, PHERO_RATE_ALPHA = 1;
   int PHERO_SPREAD_RADIUS = (int) ( STRIDE * PHERO_STRIDE_SPREAD * 0.5 ), // *0.5 to convert from diameter to radius
--- src/vax/alienantfarm/main.java
+++ src/vax/alienantfarm/main.java
@@ -28,7 +28,7 @@ public class main {
             TRACE_COLOR = Color.YELLOW.getRGB(),
             SPECIAL_COLOR = Color.GREEN.getRGB();
     AntBoard ab = new AntBoard( max_size, max_size, max_size - 1, max_size / 2, 0, max_size / 2, null );
-    AntObserver ao = new AntObserver() {
+    AntObserver image_ao = new AntObserver() {
       private int last_x, last_y;
 
       @Override
@@ -43,8 +43,8 @@ public class main {
         bi[0].setRGB( last_x, last_y, TRACE_COLOR );
         last_x = a.pos_x;
         last_y = a.pos_y;
+        bi[0].setRGB( last_x, last_y, ANT_COLOR );
         try {
-          bi[0].setRGB( last_x, last_y, ANT_COLOR );
           Thread.sleep( 50 );
         } catch (Exception ex) {
           ex.printStackTrace();
@@ -57,24 +57,60 @@ public class main {
       public void finish( AntBoard.ant a ) {
         System.out.println( "success!" );
         bi[0] = new BufferedImage( max_size, max_size, BufferedImage.TYPE_INT_ARGB );
+        for( int x = 0; x < max_size; x++ )
+          for( int y = 0; y < max_size; y++ )
+            bi[0].setRGB( x, y, ab.board[x][y].getColor() );
         img.set_image( bi[0] );
+        img.repaint();
+        try {
+          Thread.sleep( 10000 );
+        } catch (InterruptedException ex) {
+          ex.printStackTrace();
+        }
+        System.out.println( "new board..." );
+        bi[0] = new BufferedImage( max_size, max_size, BufferedImage.TYPE_INT_ARGB );
         for( int x = 0; x < max_size; x++ )
           for( int y = 0; y < max_size; y++ )
             bi[0].setRGB( x, y, ab.is_blocked( x, y ) ? BLOCK_COLOR : BOARD_COLOR );
         bi[0].setRGB( ab.exit_x, ab.exit_y, SPECIAL_COLOR );
+        img.set_image( bi[0] );
+        img.repaint();
       }
     };
-    ab.ao = ao;
+    ab.ao = image_ao;
+    ab.ao = new AntObserver() {
+
+      @Override
+      public void init( AntBoard.ant a ) {
+      }
+
+      @Override
+      public void step( AntBoard.ant a ) {
+      }
+
+      @Override
+      public void finish( AntBoard.ant a ) {
+        System.out.println( a.age );
+      }
+    };
     ab.set_block( 300, 100, 400, 600, true );
     ab.set_block( 100, 300, 600, 400, true );
+    /*
     for( int x = 0; x < max_size; x++ )
       for( int y = 0; y < max_size; y++ )
         bi[0].setRGB( x, y, ab.is_blocked( x, y ) ? BLOCK_COLOR : BOARD_COLOR );
     bi[0].setRGB( ab.exit_x, ab.exit_y, SPECIAL_COLOR );
-    //genome g = new genome( new Gene( 1, 0 ), new Gene( 1, 0 ), Gene.NULL_GENE, Gene.NULL_GENE, Gene.NULL_GENE, Gene.NULL_GENE );
+            */
+    AntBoard.genome g_pheroless = new AntBoard.genome(
+            new Gene( 1, 0 ), new Gene( 1, 0 ), Gene.NULL_GENE,
+            Gene.NULL_GENE, Gene.NULL_GENE, Gene.NULL_GENE );
+    AntBoard.genome g_pheroagg = new AntBoard.genome(
+            new Gene( 1, 0 ), new Gene( 1, 0 ), Gene.NULL_GENE,
+            new Gene( 1, 0 ), Gene.NULL_GENE, new Gene( 1, 0 ) );
+    //AntBoard.genome g = g_pheroagg;
+    //AntBoard.genome g = new AntBoard.genome( new Gene( 1, 0 ), Gene.NULL_GENE, new Gene( 1, 0 ), new Gene( 1, 0 ), new Gene( 1, 0 ), new Gene( 1, 0 ) );
     AntBoard.genome g = new AntBoard.genome();
     System.out.println( g );
-    //AntBoard.genome g = new AntBoard.genome( new Gene( 1, 0 ), Gene.NULL_GENE, new Gene( 1, 0 ), new Gene( 1, 0 ), new Gene( 1, 0 ), new Gene( 1, 0 ) );
     ab.run_iterations( 100, g );
   }
 
